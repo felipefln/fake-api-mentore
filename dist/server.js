61f384211dc4a4c14ba351ef4f7a9a405b0ba30d
felipefln <felipecarvalho_22_@hotmail.com>
2025-04-08 11:59:28 -0300

    chore: add new install json-server and downgrade npm version

--- dist/server.js
+++ dist/server.js
@@ -1,16 +1,12 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const json_server_1 = __importDefault(require("json-server"));
-const path_1 = __importDefault(require("path"));
-const server = json_server_1.default.create();
-const router = json_server_1.default.router(path_1.default.join(__dirname, 'db.json'));
-const middlewares = json_server_1.default.defaults();
+const jsonServer = require('json-server');
+const path = require('path');
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const middlewares = jsonServer.defaults();
+const PORT = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(router);
-const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`JSON Server is running on port ${PORT}`);
+    console.log(`Fake Mentore API Test to port:  ${PORT}`);
 });
--- server.ts
+++ server.ts
@@ -1,14 +1,13 @@
-import jsonServer from 'json-server';
-import path from 'path';
-
+const jsonServer = require('json-server')
+const path = require('path')
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
-
+const PORT = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(router);
 
-const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
+  console.log(`Fake Mentore API Test to port:  ${PORT}`);
 });
